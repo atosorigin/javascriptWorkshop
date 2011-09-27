b6127426991e0c7932cb635cbd020cf42903e61e
Marco Franssen <marco.franssen@gmail.com>
2011-09-27 20:13:01 +0200

    Added models for cascading dropdown

---
+++ src/JavaScript.Workshop/JavaScript.Workshop/Areas/KnockoutJS/Models/CityModel.cs
@@ -0,0 +1,8 @@
+namespace JavaScript.Workshop.Areas.KnockoutJS.Models
+{
+    public class CityModel
+    {
+        public string State { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ src/JavaScript.Workshop/JavaScript.Workshop/Areas/KnockoutJS/Models/StateModel.cs
@@ -0,0 +1,9 @@
+﻿namespace JavaScript.Workshop.Areas.KnockoutJS.Models
+{
+    public class StateModel
+    {
+        public string Country { get; set; }
+        public string StateName { get; set; }
+        public string StateCode { get; set; }
+    }
+}
