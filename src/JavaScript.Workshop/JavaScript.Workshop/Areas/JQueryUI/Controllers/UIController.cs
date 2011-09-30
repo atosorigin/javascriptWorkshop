8a5e6e47d2a91058d3e947530f2f292902a4377e
Jan Saris <jan.saris@gmail.com>
2011-09-30 09:54:54 +0200

    Prepared the first parts of chapter 3 (jQuery UI)

--- src/JavaScript.Workshop/JavaScript.Workshop/Areas/JQueryUI/Controllers/UIController.cs
+++ src/JavaScript.Workshop/JavaScript.Workshop/Areas/JQueryUI/Controllers/UIController.cs
@@ -1,16 +1,16 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
 
 namespace JavaScript.Workshop.Areas.JQueryUI.Controllers
 {
-    public class TocController : Controller
+    public class UIController : Controller
     {
-        //
-        // GET: /AdvancedJavascript/Toc/
-
         public ActionResult Index()
         {
             return View();
         }
-
     }
 }
---
+++ src/JavaScript.Workshop/JavaScript.Workshop/Scripts/workshopScripts/jQueryUI.js
@@ -0,0 +1,34 @@
+﻿//In de hoofdstukken 1 en 2 schreven we het starten van het script als volgt:
+//$(document).ready(function(){ 
+//  ExecuteOnStartup();
+//});
+
+//Zodra je op internet gaat zoeken kom je al vaker de volgende nieuwe korte notatie tegen
+//Deze notatie wordt op dit moment het meeste gebruikt.
+$(function () {
+    ExecuteOnStartup();
+});
+
+//Onder het motto: Kort-korter-korst kun je ook nog het volgende gebruiken:
+//$(ExecuteOnStartup);
+
+function ExecuteOnStartup() {
+    var config = {
+        autoOpen: false,
+        resizable: false,
+        draggable: true,
+        modal: true,
+        height: 500,
+        title: 'My dialog title',
+        buttons: { Ok: function () { $(this).dialog("close"); } },
+        open: function () { $('#dialogState').html('Dialog opened'); },
+        close: function () { $('#dialogState').html('Dialog closed'); },
+        dragStart: function () { $('#dialogState').html('Dialog drag started'); },
+        dragStop: function () { $('#dialogState').html('Dialog drag stopped'); }
+    };
+
+    $('#myDialog').dialog(config);
+    $('#openDialogButton').click(function () { $('#myDialog').dialog('open'); });
+
+    $('#myDate').datepicker({ firstDay: 1, dateFormat: 'dd-mm-yy' });
+}
