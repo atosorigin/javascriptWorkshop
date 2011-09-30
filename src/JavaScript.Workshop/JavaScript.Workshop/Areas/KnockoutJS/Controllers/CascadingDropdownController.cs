0135f4d4de9a339c430d717696d46b071eda0eaa
Marco Franssen <marco.franssen@gmail.com>
2011-09-30 20:06:27 +0200

    Fixed controller to return the cities countries etc.  Route is not working!!!!

--- src/JavaScript.Workshop/JavaScript.Workshop/Areas/KnockoutJS/Controllers/CascadingDropdownController.cs
+++ src/JavaScript.Workshop/JavaScript.Workshop/Areas/KnockoutJS/Controllers/CascadingDropdownController.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using JavaScript.Workshop.Areas.KnockoutJS.Models;
+using AttributeRouting;
 
 namespace JavaScript.Workshop.Areas.KnockoutJS.Controllers
 {
+    [RouteArea("KnockoutJS")]
     public class CascadingDropdownController : Controller
     {
         //
@@ -17,45 +18,72 @@ namespace JavaScript.Workshop.Areas.KnockoutJS.Controllers
             return View();
         }
 
+        public JsonResult Countries()
+        {
+            var countries = GetCountries();
+            return new JsonResult {Data = countries, JsonRequestBehavior = JsonRequestBehavior.AllowGet};
+        }
+
+        [GET("States/{country}")]
         public JsonResult States(string country)
         {
-            var states = GetStates().Where(s => s.Country == country);
-            return new JsonResult { Data = states, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            var states = GetStates().Where(s => s.country == country);
+            return new JsonResult {Data = states, JsonRequestBehavior = JsonRequestBehavior.AllowGet};
         }
 
+        [GET("Cities/{state}")]
         public JsonResult Cities(string state)
         {
-            var states = GetCities().Where(s => s.State == state);
-            return new JsonResult { Data = states, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            var states = GetCities().Where(s => s.state == state);
+            return new JsonResult {Data = states, JsonRequestBehavior = JsonRequestBehavior.AllowGet};
         }
 
-        private IEnumerable<CityModel> GetCities()
+        private IEnumerable<dynamic> GetCountries()
         {
-            yield return new CityModel { State = "nlli", City = "Maastricht" };
-            yield return new CityModel { State = "nlli", City = "Heerlen" };
-            yield return new CityModel { State = "nlgr", City = "Groningen" };
-            yield return new CityModel { State = "nlut", City = "Utrecht" };
-            yield return new CityModel { State = "usfl", City = "Miami" };
-            yield return new CityModel { State = "usca", City = "Los Angeles" };
-            yield return new CityModel { State = "usca", City = "San Francisco" };
+            yield return new {text = "Netherlands", value = "nl"};
+            yield return new {text = "Germany", value = "de"};
+            yield return new {text = "United States", value = "us"};
         }
 
-        private IEnumerable<StateModel> GetStates()
+        private IEnumerable<dynamic> GetStates()
         {
-            yield return new StateModel { Country = "nl", StateCode = "nlli", StateName = "Limburg" };
-            yield return new StateModel { Country = "nl", StateCode = "nlgr", StateName = "Groningen" };
-            yield return new StateModel { Country = "nl", StateCode = "nlfr", StateName = "Friesland" };
-            yield return new StateModel { Country = "nl", StateCode = "nlut", StateName = "Utrecht" };
-            yield return new StateModel { Country = "us", StateCode = "usfl", StateName = "Florida" };
-            yield return new StateModel { Country = "us", StateCode = "usne", StateName = "Nevada" };
-            yield return new StateModel { Country = "us", StateCode = "usca", StateName = "Califonia" };
-            yield return new StateModel { Country = "us", StateCode = "usar", StateName = "Arizona" };
-            yield return new StateModel { Country = "de", StateCode = "dehe", StateName = "Hesse" };
-            yield return new StateModel { Country = "de", StateCode = "deba", StateName = "Bavaria" };
-            yield return new StateModel { Country = "de", StateCode = "debr", StateName = "Brandenburg" };
-            yield return new StateModel { Country = "de", StateCode = "debe", StateName = "Berlin" };
-            yield return new StateModel { Country = "de", StateCode = "desa", StateName = "Saarland" };
-            yield return new StateModel { Country = "de", StateCode = "deha", StateName = "Hamburg" };
+            yield return new {country = "nl", value = "nlli", text = "Limburg"};
+            yield return new {country = "nl", value = "nlgr", text = "Groningen"};
+            yield return new {country = "nl", value = "nlfr", text = "Friesland"};
+            yield return new {country = "nl", value = "nlut", text = "Utrecht"};
+            yield return new {country = "us", value = "usfl", text = "Florida"};
+            yield return new {country = "us", value = "usne", text = "Nevada"};
+            yield return new {country = "us", value = "usca", text = "Califonia"};
+            yield return new {country = "us", value = "usar", text = "Arizona"};
+            yield return new {country = "de", value = "dehe", text = "Hesse"};
+            yield return new {country = "de", value = "deba", text = "Bavaria"};
+            yield return new {country = "de", value = "debr", text = "Brandenburg"};
+            yield return new {country = "de", value = "debe", text = "Berlin"};
+            yield return new {country = "de", value = "desa", text = "Saarland"};
+            yield return new {country = "de", value = "deha", text = "Hamburg"};
+        }
+
+        private IEnumerable<dynamic> GetCities()
+        {
+            yield return new {state = "nlli", text = "Maastricht", value = 10};
+            yield return new {state = "nlli", text = "Heerlen", value = 11};
+            yield return new {state = "nlgr", text = "Groningen", value = 12};
+            yield return new {state = "nlut", text = "Utrecht", value = 13};
+            yield return new {state = "usfl", text = "Miami", value = 14};
+            yield return new {state = "usca", text = "Los Angeles", value = 15};
+            yield return new {state = "usca", text = "San Francisco", value = 16};
+        }
+    }
+
+    public static class DynamicLinqExtensions
+    {
+        public static IEnumerable<dynamic> Where(this object source, Func<dynamic, bool> predicate)
+        {
+            foreach (dynamic item in source as dynamic)
+            {
+                if (predicate(item))
+                    yield return item;
+            }
         }
     }
 }
